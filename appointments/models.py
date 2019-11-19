b83bb20611a59633d91004858a3a8748e3525f7e
Erik Riedel <E.P.Riedel@gmail.com>
2019-11-19 17:11:38 +0100

    Moved appointment authentication logic to Schema

--- appointments/models.py
+++ appointments/models.py
@@ -6,32 +6,6 @@ from django.urls import reverse
 User = settings.AUTH_USER_MODEL
 
 
-def remove_sensitive_appointment(user, appointment):
-	if user != appointment.user:
-		appointment.user = None
-		appointment.title = "anonym"
-		appointment.comment = "anonym"
-		appointment.files = 0
-		#appointment.timestamp = "2019-11-18T10:39:40.461196+00:00"
-		appointment.id = 0
-
-
-def remove_sensitive_data(user, queryset):
-	for appointment in queryset:
-		remove_sensitive_appointment(user, appointment)
-
-class RestrictManager(models.Manager):
-	def by_user(self,info):
-		queryset = super(RestrictManager,self).get_queryset()
-		#print("removing sensitive Data from Entrys not by user: " + str(user))
-		print(info.context.user.is_authenticated)
-		if not info.context.user.is_authenticated:
-			raise GraphQLError('You must be logged in to see this!')
-
-		remove_sensitive_data(info.context.user,queryset)
-		return queryset
-
-
 class Appointment(models.Model):
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
 	title = models.CharField(max_length=50)
@@ -39,7 +13,6 @@ class Appointment(models.Model):
 	comment = models.CharField(max_length=200)
 	files = models.PositiveIntegerField(default=0)
 	timestamp = models.DateTimeField(auto_now=False,auto_now_add=True)
-	objects = RestrictManager()
 
 	def __str__(self):
 		return self.title
--- appointments/schema.py
+++ appointments/schema.py
@@ -1,8 +1,23 @@
 import graphene
 from graphene_django import DjangoObjectType
-
+from graphql import GraphQLError
+from graphql_jwt.decorators import login_required
 from .models import Appointment
 
+
+def remove_sensitive_appointment(user, appointment):
+	if user != appointment.user:
+		appointment.user = None
+		appointment.title = "anonym"
+		appointment.comment = "anonym"
+		appointment.files = 0
+		#appointment.timestamp = "2019-11-18T10:39:40.461196+00:00"
+		appointment.id = 0
+
+
+def remove_sensitive_data(user, queryset):
+	for appointment in queryset:
+		remove_sensitive_appointment(user, appointment)
 
 class AppointmentType(DjangoObjectType):
 	class Meta:
@@ -11,13 +26,17 @@ class AppointmentType(DjangoObjectType):
 
 class Query(graphene.ObjectType):
 	appointments = graphene.List(AppointmentType)
-
+	@login_required
 	def resolve_appointments(self, info, **kwargs):
+		queryset = Appointment.objects.all()
+		if not info.context.user.is_authenticated:
+			raise GraphQLError('You must be logged in to see this!')
+		remove_sensitive_data(info.context.user,queryset)
+		return queryset
 		return Appointment.objects.by_user(info)
 
 
 class CreateAppointment(graphene.Mutation):
-	#user =
 	id = graphene.Int()
 	timestamp = graphene.DateTime()
 
@@ -25,7 +44,7 @@ class CreateAppointment(graphene.Mutation):
 		title = graphene.String()
 		comment = graphene.String()
 		date = graphene.DateTime()
-
+	@login_required
 	def mutate(self, info, title,date, comment):
 		appointment = Appointment(user=info.context.user,title=title, comment=comment,date=date)
 		appointment.save()
