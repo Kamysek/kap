1258b4057c785d304bff4b59a4170698773dec72
Josef Kamysek <josef@kamysek.com>
2019-11-20 14:31:25 +0100

    Survey Model

--- survey/models.py
+++ survey/models.py
@@ -16,41 +16,27 @@ class Survey(models.Model):
 
 
 class Question(models.Model):
-    T = 'text'
-    C = 'choice'
-    N = 'number'
+    TEXT = 'text'
+    SELECT = 'select'
+    NUMBER = 'number'
 
-    TYPES = (
-        (T, 'text'),
-        (C, 'choice'),
-        (N, 'number'),
+    QTYPES = (
+        (TEXT, 'text'),
+        (SELECT, 'select'),
+        (NUMBER, 'number'),
     )
 
     question_text = models.CharField(max_length=1000, blank=False, null=False)
-    question_type = models.CharField(max_length=200, choices=TYPES, default=T)
     created_by = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     updated_by = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    question_type = models.CharField(max_length=200, choices=QTYPES, default=TEXT)
     survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.question_text
 
 
-class Answer(models.Model):
-    created_on = models.DateTimeField(auto_now=True)
-    created_by = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    updated_on = models.DateTimeField(auto_now=True)
-
-
-class TextAnswer(Answer):
-    answer_text = models.TextField(blank=False, null=False)
-    question = models.OneToOneField(Question, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.answer_text
-
-
-class ChoiceAnswer(Answer):
+class Choice(models.Model):
     choice_text = models.CharField(max_length=1000, blank=False, null=False)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
 
@@ -58,9 +44,31 @@ class ChoiceAnswer(Answer):
         return self.choice_text
 
 
-class NumberAnswer(Answer):
+class TextAnswer(models.Model):
+    answer_text = models.TextField(blank=False, null=False)
+    created_on = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    updated_on = models.DateTimeField(auto_now=True)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.answer_text
+
+
+class ChoiceAnswer(models.Model):
+    created_on = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    updated_on = models.DateTimeField(auto_now=True)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
+
+
+class NumberAnswer(models.Model):
     number = models.IntegerField(blank=False, null=False)
-    question = models.OneToOneField(Question, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    updated_on = models.DateTimeField(auto_now=True)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
 
     def __int__(self):
         return self.number
