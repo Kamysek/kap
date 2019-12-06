e88b6dd54a2a76e51d6ff05eb3afb6743c897948
Erik Riedel <E.P.Riedel@gmail.com>
2019-12-06 14:40:58 +0100

    Fixed requirements.txt and extended email support

--- klinischesanwendungsprojekt/utils.py
+++ klinischesanwendungsprojekt/utils.py
@@ -2,17 +2,55 @@
 import smtplib
 from email.message import EmailMessage
 
+MY_ADDRESS = "kapTest@web.de"
+PASSWORD = 'kappasswort'
+SMTP_SERVER = "smtp.web.de"
+
 def sendTestMail(recipient):
     msg = EmailMessage()
     msg.set_content("TestBody")
     msg['Subject'] = "TestSubject"
     msg['From'] = "kapTest@web.de"
-    msg['To'] = recipient
-    # Send the message via our own SMTP server.
-    username = "kapTest@web.de"
-    passwort = "kappasswort"
-    s = smtplib.SMTP('smtp.web.de')
+    msg['To'] = recipient.email
+    s = smtplib.SMTP(SMTP_SERVER)
     s.starttls()
-    s.login(username, passwort)
+    s.login(MY_ADDRESS,PASSWORD)
+    s.send_message(msg)
+    s.quit()
+
+
+def sendReminderMail(recipient):
+    msg = EmailMessage()
+    msg.set_content("Sie haben demnächst einen Termin bei uns!")
+    msg['Subject'] = "Termin Erinnerung"
+    msg['From'] = "kapTest@web.de"
+    msg['To'] = recipient.email
+    s = smtplib.SMTP(SMTP_SERVER)
+    s.starttls()
+    s.login(MY_ADDRESS, PASSWORD)
+    s.send_message(msg)
+    s.quit()
+
+def sendOverdueMail(recipient):
+    msg = EmailMessage()
+    msg.set_content("Sie nehmen bei einer Studie an unserem Institut teil und sind für eine Untersuchung überfällig!")
+    msg['Subject'] = "Termin überfällig"
+    msg['From'] = "kapTest@web.de"
+    msg['To'] = recipient.email
+    s = smtplib.SMTP(SMTP_SERVER)
+    s.starttls()
+    s.login(MY_ADDRESS, PASSWORD)
+    s.send_message(msg)
+    s.quit()
+
+def VIPreminder(vip,recipient):
+    msg = EmailMessage()
+    msg.set_content("Ein \"Very Important Patient\": " + vip.username +" hat sich für einen Termin angemeldet!")
+    msg['Subject'] = "VIP Terminanmeldung"
+    msg['From'] = "kapTest@web.de"
+    msg['To'] = recipient.email
+    s = smtplib.SMTP(SMTP_SERVER)
+    s.starttls()
+    s.login(MY_ADDRESS, PASSWORD)
     s.send_message(msg)
     s.quit()
