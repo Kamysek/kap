7116dcbc172a61afb7ec464ef22725af9208046a
Erolf345 <lol1wUQMm6lWu2Me7ia3>
2020-02-01 09:46:41 +0100

    Fixed refactoring error

--- klinischesanwendungsprojekt/crons.py
+++ klinischesanwendungsprojekt/crons.py
@@ -1,6 +1,6 @@
 from django_cron import CronJobBase, Schedule
 
-from utils.HelperMethods import checkAllUsersOverdue, doAppointmentReminders
+import utils.HelperMethods
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
@@ -13,11 +13,10 @@ User = get_user_model()
 class MyCronJob(CronJobBase):
     RUN_EVERY_MINS = 360 # every 6 hours
     RUN_AT_TIMES = ['1:00', '12:30']
-
     # schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
     schedule = Schedule(run_at_times=RUN_AT_TIMES)
     code = 'my_app.my_cron_job'    # a unique code
 
     def do(self):
-        doAppointmentReminders()
-        checkAllUsersOverdue()
+        utils.HelperMethods.doAppointmentReminders()
+        utils.HelperMethods.checkAllUsersOverdue()
