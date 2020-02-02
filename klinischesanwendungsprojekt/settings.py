a1ecdcdec700e898f425cba9e07c19fe7acfb96d
Erolf345 <lol1wUQMm6lWu2Me7ia3>
2020-02-02 19:29:21 +0100

    added postgres and removed graphiql for production,

--- klinischesanwendungsprojekt/settings.py
+++ klinischesanwendungsprojekt/settings.py
@@ -97,10 +97,19 @@ WSGI_APPLICATION = 'klinischesanwendungsprojekt.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+    'postgres': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'kaPostgres',
+        'USER': 'kap',
+        'PASSWORD': 'kap',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
