152946aa88dcdb6489939866dbf4f1045ac9a332
Erolf345 <lol1wUQMm6lWu2Me7ia3>
2020-02-02 11:44:43 +0100

    use testBackend for emails

--- klinischesanwendungsprojekt/settings.py
+++ klinischesanwendungsprojekt/settings.py
@@ -32,7 +32,7 @@ EMAIL_HOST_PASSWORD = 'kappasswort'
 EMAIL_HOST = "smtp.web.de"
 EMAIL_USE_TLS = True
 EMAIL_TIMEOUT = 5
-#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 GRAPHQL_JWT = {
     'JWT_VERIFY_EXPIRATION': True,
